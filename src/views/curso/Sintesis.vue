a7e27bde1d1025a018f67828fa5697b0476a64b8
Francisco Lizcano <flizcanor@icloud.com>
2025-06-02 19:15:25 -0500

    se ajusta sintesis

--- src/views/curso/Sintesis.vue
+++ src/views/curso/Sintesis.vue
@@ -2,7 +2,7 @@
   .curso-main-container.creditos-vista
     BannerInterno(subTitulo="SÍNTESIS")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-      p(data-aos="fade-up").mb-5 La Unidad 3: Bioética y sostenibilidad en el desarrollo económico global, establece los principios esenciales para comprender cómo los valores éticos y bioéticos pueden orientar el desarrollo económico hacia la sostenibilidad. Esta unidad proporciona una perspectiva integral sobre la relación entre la economía, el medio ambiente y los derechos humanos, destacando la importancia de prácticas responsables en contextos globalizados. Este enfoque asegura que los estudiantes adquieran una comprensión sólida de los desafíos éticos actuales y las estrategias sostenibles, permitiéndoles aplicar estas herramientas de manera efectiva en su formación profesional y en la vida cotidiana.
+      p(data-aos="fade-up").mb-5 La unidad 3: Impacto de la IA en la sociedad y la ética, permite comprender las implicaciones sociales y éticas derivadas del uso creciente de la inteligencia artificial en sectores como el trabajo, la salud, la justicia y la educación. A través del análisis de sus beneficios, desafíos y efectos sobre la equidad, la privacidad y la responsabilidad, esta unidad promueve una reflexión crítica sobre el desarrollo tecnológico. Este enfoque brinda a los estudiantes herramientas para participar activamente en la construcción de sistemas de IA más justos, transparentes y centrados en el bienestar humano.
 
       .row.justify-content-center
         .col-lg-12.mb-5
